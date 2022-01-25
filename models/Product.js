3098b81e5d8d381eb2b5c3c52b7fc2e5896e8da2
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-24 22:16:40 -0600

    update-api

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    picture: String,
-    price: Number,
-    desc : String
+    name: {
+        type: String,
+        require:[true, 'Please type a name'],
+        unique: true,
+        trim: true,
+    }
 })
 
 module.exports = mongoose.models.Product||mongoose.model('Product',ProductSchema)
